bac881c13e3919c893662ed23db65326669e10eb
Storm <sangolts@gmail.com>
2025-01-27 14:34:10 +0100

    Oblig 1

---
+++ app/src/main/java/no/dat153/thequizapp/MainActivity.java
@@ -0,0 +1,39 @@
+package no.dat153.thequizapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import no.dat153.thequizapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        EdgeToEdge.enable(this);
+        // setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(view, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.buttonStartQuiz.setOnClickListener(v -> {
+            Log.d("MainActivity", "Launching QuizActivity");
+            startActivity(new Intent(this, QuizStartActivity.class));
+        });
+    }
+}
